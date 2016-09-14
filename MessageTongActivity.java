8974a42ce31dd61adddfd4865edff6694743ec5c
姬鑫 <274921407@qq.com>
2016-09-14 13:41:36 +0800

    界面类
    消息提示音界面

---
+++ MessageTongActivity.java
@@ -0,0 +1,104 @@
+package com.jixin.studyproject.ui;
+
+import android.app.Activity;
+import android.content.Context;
+import android.database.Cursor;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.jixin.studyproject.R;
+import com.jixin.studyproject.adapter.MessageTongListAdapter;
+import com.jixin.studyproject.bean.MessageTongBean;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by jixin on 2016/9/13
+ */
+public class MessageTongActivity extends Activity {
+
+
+    private ListView msgtong_list;
+    private Button save_bt;
+
+    private List<MessageTongBean> mList;
+    private MessageTongListAdapter mAdapter;
+
+    private RingtoneManager rm;
+
+    private Uri defaultUri;
+    private Ringtone defaultRingtong;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messagetong);
+        msgtong_list = (ListView) findViewById(R.id.msgtong_list);
+        save_bt = (Button) findViewById(R.id.save_bt);
+        initDate();
+        msgtong_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                for (int i = 0; i < mList.size(); i++) {
+                    mList.get(i).setIsSelected(false);
+                }
+                mList.get(position).setIsSelected(true);
+                mAdapter.notifyDataSetChanged();
+
+                if (position == 0) {
+                    if (defaultRingtong.isPlaying()) {
+                        defaultRingtong.stop();
+                    }
+                    defaultRingtong.play();
+                } else {
+                    Ringtone rt = rm.getRingtone(position);
+                    if (rt.isPlaying()) {
+                        rt.stop();
+                    }
+                    rt.play();
+                }
+            }
+        });
+    }
+
+
+    private void initDate() {
+
+        mList = new ArrayList<>();
+        rm = new RingtoneManager(getApplicationContext());
+        rm.setType(RingtoneManager.TYPE_NOTIFICATION);
+        defaultUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        defaultRingtong = RingtoneManager.getRingtone(this, defaultUri);
+        MessageTongBean defaultRtb = new MessageTongBean();
+        //defaultRtb.setMsgTongName(defaultRingtong.getTitle(this));
+        defaultRtb.setMsgTongName("跟随系统");
+        defaultRtb.setIsSelected(true);
+        mList.add(defaultRtb);
+        Cursor cs = rm.getCursor();
+        int count = cs.getCount();
+        for (int i = 0; i < count; i++) {
+            Ringtone rt = rm.getRingtone(i);
+            MessageTongBean tmp = new MessageTongBean();
+            tmp.setMsgTongName(rt.getTitle(this));
+            tmp.setIsSelected(false);
+            mList.add(tmp);
+        }
+
+
+        mAdapter = new MessageTongListAdapter(mList, this);
+        msgtong_list.setAdapter(mAdapter);
+    }
+
+
+}
